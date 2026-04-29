95fc659b98642e54ce43876f85eaa8a5e208f800
Justin Fan <justinfan@ucsb.edu>
2026-04-28 20:18:00 -0700

    created ucsbDiningCommonsMenuItemsFictures.js

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemsFixtures = {
+  oneDiningCommonsMenuItem: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "Baked Pesto Pasta with Chicken",
+    station: "Entree Specials",
+  },
+  threeDiningCommonsMenuItem: [
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Baked Pesto Pasta with Chicken",
+      station: "Entree Specials",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "ortega",
+      name: "Tofu Banh Mi Sandwich (v)",
+      station: "Entree Specials"
+    },
+    {
+      id: 3,
+      diningCommonsCode: "portola",
+      name: "Cream of Broccoli Soup (v)",
+      station: "Greens & Grains"
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemsFixtures };
