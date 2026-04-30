d648ebdb9e643ee7195b8caa03bbaad580975565
Justin Fan <justinfan@ucsb.edu>
2026-04-29 17:50:35 -0700

    Created tables component for UCSBDiningCommonsMenuItems and tests and story

---
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemsTable.stories.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import UCSBDiningCommonsMenuItemsTable from "main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsTable";
+import { ucsbDiningCommonsMenuItemsFixtures } from "fixtures/ucsbDiningCommonsMenuItemFixtures";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { http, HttpResponse } from "msw";
+
+export default {
+  title:
+    "components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsTable",
+  component: UCSBDiningCommonsMenuItemsTable,
+};
+
+const Template = (args) => {
+  return <UCSBDiningCommonsMenuItemsTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  diningCommonsMenuItems: [],
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+  diningCommonsMenuItems: ucsbDiningCommonsMenuItemsFixtures.threeDiningCommonsMenuItems,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+  diningCommonsMenuItems: ucsbDiningCommonsMenuItemsFixtures.threeDiningCommonsMenuItems,
+  currentUser: currentUserFixtures.adminUser,
+};
+
+ThreeItemsAdminUser.parameters = {
+  msw: [
+    http.delete("/api/ucsbdiningcommonsmenuitems", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
