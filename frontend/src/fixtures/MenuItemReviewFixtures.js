21842169d5365b0d5d6b65600893553e99bf1a48
Kai Li <kaijunli@ucsb.edu>
2026-04-30 17:34:45 -0700

    Kai fix code formatting

--- frontend/src/fixtures/MenuItemReviewFixtures.js
+++ frontend/src/fixtures/MenuItemReviewFixtures.js
@@ -1,37 +1,37 @@
 const MenuItemReviewFixtures = {
   oneMenuItemReview: {
-        "id" : 1,
-        "itemId" : 17,
-        "reviewerEmail" : "kaijunli@ucsb.edu",
-        "stars" : 5,
-        "dateReviewed": "2022-01-02T12:00:00",
-        "comments" : "skibidi"
-    },
+    id: 1,
+    itemId: 17,
+    reviewerEmail: "kaijunli@ucsb.edu",
+    stars: 5,
+    dateReviewed: "2022-01-02T12:00:00",
+    comments: "skibidi",
+  },
   threeMenuItemReviews: [
     {
-        "id" : 1,
-        "itemId" : 17,
-        "reviewerEmail" : "kaijunli@ucsb.edu",
-        "stars" : 5,
-        "dateReviewed": "2022-01-02T12:00:00",
-        "comments" : "skibidi"
+      id: 1,
+      itemId: 17,
+      reviewerEmail: "kaijunli@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2022-01-02T12:00:00",
+      comments: "skibidi",
     },
     {
-        "id" : 2,
-        "itemId" : 69,
-        "reviewerEmail" : "testing@ucsb.edu",
-        "stars" : 4,
-        "dateReviewed": "2023-01-02T12:00:00",
-        "comments" : "skibidigyatt"
+      id: 2,
+      itemId: 69,
+      reviewerEmail: "testing@ucsb.edu",
+      stars: 4,
+      dateReviewed: "2023-01-02T12:00:00",
+      comments: "skibidigyatt",
     },
     {
-        "id" : 3,
-        "itemId" : 67,
-        "reviewerEmail" : "secondtest@ucsb.edu",
-        "stars" : 2,
-        "dateReviewed": "2024-01-02T12:00:00",
-        "comments" : "bruh"
-    }
+      id: 3,
+      itemId: 67,
+      reviewerEmail: "secondtest@ucsb.edu",
+      stars: 2,
+      dateReviewed: "2024-01-02T12:00:00",
+      comments: "bruh",
+    },
   ],
 };
 
