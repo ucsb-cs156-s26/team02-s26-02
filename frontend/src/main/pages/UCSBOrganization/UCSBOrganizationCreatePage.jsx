9b09f577a6e4ce8c805832988a7a017091d4b7d6
Kai Li <kaijunli@ucsb.edu>
2026-05-05 15:53:41 -0700

    fixed eslint

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.jsx
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.jsx
@@ -1,8 +1,8 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import RestaurantForm from "main/components/Restaurants/RestaurantForm";
-import { Navigate } from "react-router";
-import { useBackendMutation } from "main/utils/useBackend";
-import { toast } from "react-toastify";
+import _RestaurantForm from "main/components/Restaurants/RestaurantForm";
+import { _Navigate } from "react-router";
+import { _useBackendMutation } from "main/utils/useBackend";
+import { _toast } from "react-toastify";
 
 export default function UCSBOrganizationCreatePage() {
   return (
