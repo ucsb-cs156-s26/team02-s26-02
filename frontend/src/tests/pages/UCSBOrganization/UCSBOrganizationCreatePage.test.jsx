899e921547e76fb8cc9199b32a875a9c30ec5317
Kevin Zhao <kevinzhao@ucsb.edu>
2026-05-04 23:31:11 -0700

    Added UCSBOrganization Create Page with 100% tests.

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, _fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
@@ -57,8 +57,80 @@ describe("UCSBOrganizationCreatePage tests", () => {
 
     await waitFor(() => {
       expect(
-        screen.getByText("Create Page not yet implemented"),
+        screen.getByText("Create New UCSBOrganization"),
       ).toBeInTheDocument();
     });
+    await waitFor(() => {
+      expect(screen.getByLabelText("orgCode")).toBeInTheDocument();
+    });
+  });
+
+  test("on submit, makes request to backend, and redirects to /ucsborganization", async () => {
+    const queryClient = new QueryClient();
+    const newOrganization = {
+      orgCode: "ANI",
+      orgTranslationShort: "Anime Club",
+      orgTranslation: "Japanese Animation Club",
+      inactive: false,
+    };
+
+    axiosMock.onPost("/api/ucsborganization/post").reply(202, newOrganization);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <UCSBOrganizationCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByLabelText("orgCode")).toBeInTheDocument();
+    });
+
+    const orgCodeInput = screen.getByLabelText("orgCode");
+    expect(orgCodeInput).toBeInTheDocument();
+
+    const orgTranslationShortInput = screen.getByLabelText(
+      "orgTranslationShort",
+    );
+    expect(orgTranslationShortInput).toBeInTheDocument();
+
+    const orgTranslationInput = screen.getByLabelText("orgTranslation");
+    expect(orgTranslationInput).toBeInTheDocument();
+
+    const inactiveInput = screen.getByLabelText("inactive");
+    expect(inactiveInput).toBeInTheDocument();
+
+    const createButton = screen.getByText("Create");
+    expect(createButton).toBeInTheDocument();
+
+    fireEvent.change(orgCodeInput, { target: { value: "ANI" } });
+    fireEvent.change(orgTranslationShortInput, {
+      target: { value: "Anime Club" },
+    });
+    fireEvent.change(orgTranslationInput, {
+      target: { value: "Japanese Animation Club" },
+    });
+    fireEvent.change(inactiveInput, {
+      target: { value: false },
+    });
+
+    fireEvent.click(createButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      orgCode: "ANI",
+      orgTranslationShort: "Anime Club",
+      orgTranslation: "Japanese Animation Club",
+      inactive: "false",
+    });
+
+    // assert - check that the toast was called with the expected message
+    expect(mockToast).toBeCalledWith(
+      "New UCSBOrganization Created - orgCode: ANI org Translation Short: Anime Club",
+    );
+    expect(mockNavigate).toBeCalledWith({ to: "/ucsborganization" });
   });
 });
