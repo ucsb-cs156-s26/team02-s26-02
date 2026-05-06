a9a40b6bcb70ea5bca96a65165529ea38e83006e
Kai Li <kaijunli@ucsb.edu>
2026-05-06 13:14:36 -0700

    clamped stars and fixed merge

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -83,6 +83,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/MenuItemReview">
                     MenuItemReview
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/articles">
+                    Articles
+                  </Nav.Link>
                 </>
               ) : (
                 <></>
