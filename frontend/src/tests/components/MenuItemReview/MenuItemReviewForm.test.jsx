16bb00d85cbc5610b831d1986c136c9a1a4c7201
Kai Li <kaijunli@ucsb.edu>
2026-05-06 13:28:57 -0700

    add tests for clamp

--- frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.jsx
+++ frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, getByLabelText, getByTestId, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router";
 
 import MenuItemReviewForm from "main/components/MenuItemReview/MenuItemReviewForm";
@@ -101,4 +101,26 @@ describe("MenuItemReviewForm tests", () => {
     expect(screen.getByText(/dateReviewed is required/)).toBeInTheDocument();
     expect(screen.getByText(/comments is required/)).toBeInTheDocument();
   });
+  test("stars boundary", async () => {
+    render(
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <MenuItemReviewForm />
+            </Router>
+        </QueryClientProvider>,
+    );
+
+    const starsInput = screen.getByTestId("MenuItemReviewForm-stars");
+    const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
+
+    fireEvent.change(starsInput, { target: { value: '6' } });
+    fireEvent.click(submitButton);
+
+    expect(await screen.findByText(/Maximum Rating is 5/)).toBeInTheDocument();
+
+    fireEvent.change(starsInput, { target: { value: '-1' } });
+    fireEvent.click(submitButton);
+
+    expect(await screen.findByText(/Minimum Rating is 0/)).toBeInTheDocument();
+});
 });
