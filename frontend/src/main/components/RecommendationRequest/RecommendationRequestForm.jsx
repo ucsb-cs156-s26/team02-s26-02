d033b2295ffc0d1dc64c3cf0de204ef186f13ac7
Kevin Zhao <kevinzhao@ucsb.edu>
2026-05-05 18:46:14 -0700

    update regex for form

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.jsx
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.jsx
@@ -23,7 +23,7 @@ function RecommendationRequestFor({
   // Stryker disable Regex
   const isodate_regex =
     /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
-  const email_regex = /^[\w-.]+@[.]+$/;
+  const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   // Stryker restore Regex
 
   return (
